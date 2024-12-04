ed7576af94a88ed9432b2215bfca4d42ab246b76
Aranfraij <aran.fraij@post.runi.ac.il>
2024-12-04 17:54:18 +0200

    Update MyString.java

--- MyString.java
+++ MyString.java
@@ -1,75 +1,84 @@
-
 public class MyString {
-
     public static void main(String[] args) {
         System.out.println("Testing lowercase:");
         System.out.println("UnHappy : " + lowerCase("UnHappy"));
         System.out.println("This costs 15 Sheksls : " + lowerCase("This costs 15 Sheksls"));
         System.out.println("TLV : " + lowerCase("TLV"));
-        System.out.println("lowercase : " + lowerCase("lowercase"));
-
+        System.out.println("EaSy PeAsY LeMoN SqUeEzy : " + lowerCase("EaSy PeAsY LeMoN SqUeEzy"));
+       
         System.out.println("Testing contains:");
-        // System.out.println(contains("unhappy", "happy")); // true
-        // System.out.println(contains("happy", "unhappy")); // false
-        // System.out.println(contains("historical", "story")); // false
-        // System.out.println(contains("psychology", "psycho")); // true
-        // System.out.println(contains("personality", "son")); // true
-        // System.out.println(contains("personality", "dad")); // false
-        // System.out.println(contains("resignation", "sign")); // true      
-
-        // System.out.println(contains("baba yaga", "baba"));
-        // System.out.println(contains("baba yaga", ""));
-        // System.out.println(contains("baba yaga", "John Wick is the baba yaga"));
-        // System.out.println(contains("baba yaga", "Yaga"));
-        System.out.println(contains("baba yaga", "babayaga"));
+        System.out.println(contains("unhappy", "happy")); // true
+        System.out.println(contains("happy", "unhappy")); // false
+        System.out.println(contains("historical", "story")); // false
+        System.out.println(contains("psychology", "psycho")); // true
+        System.out.println(contains("personality", "son")); // true
+        System.out.println(contains("personality", "dad")); // false
+        System.out.println(contains("resignation", "sign")); // System.out.println(index1("resignation", "sign")); // true
     }
-
-    /**
-     * Returns the lowercase version of the given string.
-     */
+    /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-        String lowStr = "";
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) >= 65 && str.charAt(i) <= 90) {
-                lowStr += (char) (str.charAt(i) + 32);
-            } else {
-                lowStr += str.charAt(i);
-            }
+        String lowString= "";
+        if (str == "") {
+            return "";
         }
-        return lowStr;
+        for(int i=0; i<str.length(); i++){
+        if (str.charAt(i) == ' ') {
+            lowString+= " ";
+        }
+        else {
+           if ((int)(str.charAt(i)) < 91) {
+                lowString += (char)(str.charAt(i) + 32);   
+           }
+           else
+           lowString += str.charAt(i);
+        }
+        }
+        return lowString;
     }
-
-    /**
-     * If str1 contains str2, returns true; otherwise returns false.
-     */
-    public static boolean contains(String str1, String str2) {
-        //str1 = lowerCase(str1);
-        //str2 = lowerCase(str2);
-        if (str2.length() > str1.length()) {
+public static int index2(String str1, String str2) {
+    for(int i=0; i< str1.length(); i++){
+        for(int j=0; j<str2.length(); j++)
+            if (str2.charAt(j) == str1.charAt(i)){
+               return j;
+        }    
+    }
+    return -1;
+}  
+public static boolean contains(String str1, String str2) {
+    if (str2.length() > str1.length()) {
+        return false;
+    }
+    if (str2 == "") {
+        return true;
+    }
+    if (str1.length() == str2.length()) {
+      for(int i=0; i<str1.length(); i++){
+        if (str1.charAt(i) != str2.charAt(i)) {
             return false;
         }
-        if (str2.length() == 0) {
+        if (i == str1.length()-1) {
+            return true;            
+        }
+      }
+       }
+    int firstindex = -1; 
+      for(int i=0; i<str1.length(); i++){
+        if (str1.charAt(i) == str2.charAt(0)) {
+            firstindex = i;
+            break;
+        }
+        if (firstindex == -1 && i == str1.length()-1) {
+            return false;
+        }
+      }
+      for(int j = 0; j < str2.length(); j++){ 
+        if (str1.charAt(j + firstindex) != str2.charAt(j)) {
+            return false;
+        }
+        if (str1.charAt(j+firstindex) == str2.charAt(j) && j == str2.length()-1) {
             return true;
         }
-        int index = 0;
-        String subStr = "";
-        for (int i = 0; i < str1.length(); i++) {
-            if (str1.charAt(i) == str2.charAt(index)) {
-                //System.out.println(str1.charAt(i) + " ," + str2.charAt(index));
-                subStr += str2.charAt(index);
-                if (index + 1 < str2.length()) {
-                    index++;
-                } // else {
-                //     System.out.println(subStr);
-                // }
-                else if (str1.charAt(i) != str2.charAt(index)) {
-                    System.out.println(str1.charAt(i) + ", " + str2.charAt(index));
-                    return false;
-                } else {
-                    break;
-                }
-            }
-        }
-        return (str2.equals(subStr));
-    }
+      }
+      return false;
+}
 }
