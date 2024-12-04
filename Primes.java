c3862b5a736e5809b9f3d8de105b375d11c6a174
Aranfraij <aran.fraij@post.runi.ac.il>
2024-12-04 17:45:12 +0200

    Update Primes.java

--- Primes.java
+++ Primes.java
@@ -1,40 +1,26 @@
-
 public class Primes {
-
     public static void main(String[] args) {
-        int lim = Integer.parseInt(args[0]);
-        int j = 2;
-        boolean[] bool = new boolean[lim + 1];
-        for (int i = 0; i < bool.length; i++) { // if the index>1 it is true
-            if (i > 1) {
-                bool[i] = true;
-            }
-            // System.out.printf("%8s", bool[i]);
+        System.out.println( "Prime numbers up to " + (args [0] + ":"));
+        int bound = Integer.parseInt(args [0]);
+        int count = 0;
+        int i = 2;
+        while (i <= bound) {
+            if (i == 2) {
+                System.out.println(i);
+                count ++;
+            } else {
+                int num = i -1;
+                while (i % num != 0) {
+                    num --;
+                    if (num == 1) {
+                        count ++;
+                        System.out.println(i);
+                        break;
+                    }
+                }
+            } i++;
         }
-        //System.out.println();
-        for (int i = 2; i <= bool.length; i++) {
-            while (i % j == 0 && i > j) {
-                bool[i] = false;
-                //System.out.println(bool[i]);
-                break;
-            }
-            if (i + 1 == lim) {
-                j++;
-                i = j + 1;
-            }
-        }
+        System.out.println("There are " + count + " primes between 2 and " + bound + " (" + ((count * 100) / bound) + "% are primes)");
 
-        double countP = 0;
-        for (int i = 0; i < bool.length; i++) {
-            // System.out.printf("%8s", bool[i]);
-            if (bool[i] == true) {
-                countP++;
-            }
-        }
-
-        double presentage = (countP / lim) * 100;
-        //System.out.println();
-        System.out.println("Prime numbers up to " + lim + ":");
-        System.out.println("There are " + (int) countP + " primes between 2 and " + lim + " (" + (int) presentage + "% are primes) ");
     }
 }
